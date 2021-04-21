de7716aa74edb5dc4cfac346a3f76583e7e43f97
Marihbj <mh.bjordal@stud.uis.no>
2021-04-21 13:02:49 +0200

    Layout inni artikler for kommende turer tilpasset

--- static/src/articleC.js
+++ static/src/articleC.js
@@ -3,8 +3,12 @@ let articleC = {
     template: /*html*/`
     <div class="outer">
         <div class="card front">
-            <p>By</p>
-            <p>{{article}}</p>
+            <img :src="'https://images.unsplash.com/photo-1618023217167-088530104197?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1652&q=80'" alt="ScheduledBanner"/>
+            <div class="description">
+                <h3>Tokyo, Japan</h3>
+                <p>By nr: {{article}}</p>
+                <p>Description for this trip. What a cool destination. But what happens to the box when you add a lot of text. If your an asshole and put to much, what happens? Maybe put a limit in db?</p>
+            </div>
         </div>
     </div>
     `,
