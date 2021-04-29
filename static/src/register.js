501ea73829ad5e262a3ef1c9ee23e0f622402249
Marihbj <mh.bjordal@stud.uis.no>
2021-04-29 20:09:11 +0200

    Register html og css

--- static/src/register.js
+++ static/src/register.js
@@ -1,43 +1,29 @@
 let registerform = {
-    template: `
-    <div>
-        <div class="content">
-            <p>Test</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>Test</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>Test</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>Test</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>Test</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
-            <p>test register</p>
+    template: /*html*/`
+        <div>
+            <div class="content">
+                <main class="main">
+                    <div class="registerbox">
+                        <div class="loginform registerform">
+                            <img id="smallLogo" src="static/images/travelLogo.png" alt="Logo"/>
+                            <h2>Register</h2>
+                            <form @submit="">
+                                <input id="username" v-model="registerData.username" placeholder="Username" required/><br>
+                                <input type="password" id="password" v-model="registerData.password" placeholder="Password" required/><br>
+                                <button id="registerbutton" type="button" @click="">Register</button>
+                            </form>
+                        </div>
+                    </div>
+            </main>
         </div>
     </div>
-    `
+    `,
+    data: function() {
+        return {
+            registerData: {
+                username: "",
+                password: "",
+            }
+        }
+    }
 }
